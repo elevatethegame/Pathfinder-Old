85a24eaea26c1b72821a4f5663252f0df0384706
Bruce Ong <bruceskellator@hotmail.com>
2021-02-24 12:35:12 +0800

    Changed onAnimationEnd to onAnimationStart so that path renders immediately after end node has been reached.

--- src/PathfinderApp/Grid/myNode/myNode.js
+++ src/PathfinderApp/Grid/myNode/myNode.js
@@ -4,7 +4,7 @@ function MyNode(props) {
   return (
     <td className={props.className} 
         style={{animationDelay: `${props.delay}ms`}}
-        onAnimationEnd={() => {
+        onAnimationStart={() => {
           // Once the end node has been visited, trigger the path animation
           if (props.isEndNode) {
             console.log("On Animation End");
