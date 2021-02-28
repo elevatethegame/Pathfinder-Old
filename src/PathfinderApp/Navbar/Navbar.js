5199be72c21a4de42ff1d4520767fedba3d94b56
Bruce Ong <bruceskellator@hotmail.com>
2021-02-28 22:33:00 +0800

    Finished styling drop down sub menu for navbar.

--- src/PathfinderApp/Navbar/Navbar.js
+++ src/PathfinderApp/Navbar/Navbar.js
@@ -13,9 +13,17 @@ class Navbar extends Component {
     return (
       <div className="navContainer">
       <ul className="navBar">
-          <li className="navItem"><span>Pick an Algorithm</span></li>
+          <li className="navItem">
+            <span>Pick an Algorithm</span>
+            <ul className="subMenu">
+              <li className="subMenuItem"><span>A Star Search</span></li>
+              <li className="subMenuItem"><span>Breadth First Search</span></li>
+              <li className="subMenuItem"><span>Depth First Search</span></li>
+            </ul>
+          </li>
           <li className="navItem" onClick={() => this.props.onClickSearch(this.state.selectedAlgorithm)}><span>Start Search!</span></li>
           <li className="navItem" onClick={this.props.onClickGenWalls}><span>Generate Walls!</span></li>
+          <li className="navItem"><span>Learn the Algos</span></li>
       </ul>
       </div>
     );
